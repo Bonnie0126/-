2aca0614a68fe6fbab1236788378b00ee6eeae9b
Bonnie0126 <l>
2020-05-30 17:23:21 +0800

    构建分类器

--- 2.KNN/kNN.py
+++ 2.KNN/kNN.py
@@ -1,106 +1,25 @@
 from numpy import*
 import operator
-
+#定义KNN算法分类器函数
+#函数参数包括：（测试数据，训练数据，分类，K值）
+def classify(inX,dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX,(dataSetSize,1))-dataSet
+    sqDiffMat=diffMat**2
+    sqDistances=sqDiffMat.sum(axis=1)
+    distances=sqDistances**0.5 #计算欧式距离
+    sortedDistIndicies=distances.argsort() #从小到大进行排序并返回index
+    #选择距离最近的k个值
+    classCount={}
+    for i in range(k):
+        voteIlabel=labels[sortedDistIndicies[i]]
+        #D.get(k[,d]) -> D[k] if k in D, else d. d defaults to None.
+        classCount[voteIlabel]=classCount.get(voteIlabel,0)+1
+    #排序
+    sortedClassCount=sorted(classCount.items(),key=operator.itemgetter(1),reverse=True)
+    return sortedClassCount[0][0]#返回发生频率最高的元素标签
+#定义生成“训练样本集”的函数，包含特征和分类信息
 def createDataSet():
 	group = array([[1.0,1.1],[1.0,1.1],[0,0],[0,0.1]])
 	labels = ['A','A','B','B']
 	return group, labels
-
-def classify0(inX, dataSet, labels, k):  #classify0有4个输入参数：用于分类的输入
-向量inX,输入的训练样本集dataSet，标签向量labels，k表示选择最近邻的述目
-    dataSetSize = dataSet.shape[0] 
-    diffMat = tile(inX, (dataSetSize,1)) - dataSet
-    sqDiffMat = diffMat**2
-    sqDistances = sqDiffMat.sum(axis=1)
-    distances = sqDistances**0.5             #这部分代码计算向量点xA和xB之间的距离
-     d=根号下（xA0-xB0）平方+（xA1-xB1）平方
-    sortedDistIndicies = distances.argsort() #对距离按照从小到大的顺序排序
-    classCount = {}
-    for i in range(k):
-        voteIlabel = labels[sortedDistIndicies[i]]
-        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1   #确定前k个
-        距离最小元素所在的主要分类，输入的k总时正整数
-    sortedClassCount = sorted(classCount.items(),key=operator.itemgetter(1),
-    reverse = True) #将classCount字典分解为元组列表，然后使用itemgetter方法，
-    按照第二个元素的次序对元组进行排序，此处的排序为逆序
-    #在python3报错，将iteritems改为items即可,iteritems是为python2环境中dict的函数
-    return sortedClassCount[0][0]  #返回发生频率最高的元素标签
-
-
-def createDataSet():
-    group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
-    labels = ['A','A','B','B']
-    return group, labels
-
-def file2matrix(filename):
-    fr = open(filename)
-    numberOfLines = len(fr.readlines())         #get the number of lines in the file
-    returnMat = zeros((numberOfLines,3))        #prepare matrix to return
-    classLabelVector = []                       #prepare labels return   
-    fr = open(filename)
-    index = 0
-    for line in fr.readlines():
-        line = line.strip()
-        listFromLine = line.split('\t')
-        returnMat[index,:] = listFromLine[0:3]
-        classLabelVector.append(int(listFromLine[-1]))
-        index += 1
-    return returnMat,classLabelVector
-    
-def autoNorm(dataSet):
-    minVals = dataSet.min(0)
-    maxVals = dataSet.max(0)
-    ranges = maxVals - minVals
-    normDataSet = zeros(shape(dataSet))
-    m = dataSet.shape[0]
-    normDataSet = dataSet - tile(minVals, (m,1))
-    normDataSet = normDataSet/tile(ranges, (m,1))   #element wise divide
-    return normDataSet, ranges, minVals
-   
-def datingClassTest():
-    hoRatio = 0.50      #hold out 10%
-    datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')       #load data setfrom file
-    normMat, ranges, minVals = autoNorm(datingDataMat)
-    m = normMat.shape[0]
-    numTestVecs = int(m*hoRatio)
-    errorCount = 0.0
-    for i in range(numTestVecs):
-        classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
-        print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i])
-        if (classifierResult != datingLabels[i]): errorCount += 1.0
-    print "the total error rate is: %f" % (errorCount/float(numTestVecs))
-    print errorCount
-    
-def img2vector(filename):
-    returnVect = zeros((1,1024))
-    fr = open(filename)
-    for i in range(32):
-        lineStr = fr.readline()
-        for j in range(32):
-            returnVect[0,32*i+j] = int(lineStr[j])
-    return returnVect
-
-def handwritingClassTest():
-    hwLabels = []
-    trainingFileList = listdir('trainingDigits')           #load the training set
-    m = len(trainingFileList)
-    trainingMat = zeros((m,1024))
-    for i in range(m):
-        fileNameStr = trainingFileList[i]
-        fileStr = fileNameStr.split('.')[0]     #take off .txt
-        classNumStr = int(fileStr.split('_')[0])
-        hwLabels.append(classNumStr)
-        trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)
-    testFileList = listdir('testDigits')        #iterate through the test set
-    errorCount = 0.0
-    mTest = len(testFileList)
-    for i in range(mTest):
-        fileNameStr = testFileList[i]
-        fileStr = fileNameStr.split('.')[0]     #take off .txt
-        classNumStr = int(fileStr.split('_')[0])
-        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
-        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
-        print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr)
-        if (classifierResult != classNumStr): errorCount += 1.0
-    print "\nthe total number of errors is: %d" % errorCount
-    print "\nthe total error rate is: %f" % (errorCount/float(mTest))
